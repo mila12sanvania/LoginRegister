b32789ed5d587b50d4e13972b359fa798b8e701d
mila12sanvania <milarpl17@gmail.com>
2023-06-11 12:37:19 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Registrasi from "./pages/Registrasi";
+export const apiEndPoint = "https://647c25b9c0bae2880ad06ad3.mockapi.io/";
+
+function App() {
+	return (
+		<div style={{ fontFamily: "Poppins, sans-serif" }}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/registrasi" element={<Registrasi />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
